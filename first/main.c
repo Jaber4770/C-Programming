79880129969e464ae80d0ba5034d63da0ebf1d2c
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-05-10 23:30:15 +0200

    c programming to onk perar.type niye bohot somossa te ache. bar bar bole dite hochhe.kon type. ekbar bolleo hoina 3 jaigai bola lagse. ki ekta somossa!

--- first/main.c
+++ first/main.c
@@ -1,12 +1,41 @@
-
-
-
 #include <stdio.h>
 
  int main(){
- int a,b;
- printf("enter tow numbers: ");
+ // int a,b;
+ //printf("enter tow numbers: ");
  printf("here we are taking number from you. \n why we are taking these? \n hodai nitesi bhai. kono karon chra. ");
- scanf("%d %d", &a,&b);
+ //scanf("%d %d", &a,&b);
+
+ int myNumber = 5;
+ printf("%d",myNumber);
+
+ float myfloatNum = 343334.3434;
+ printf("%f",myfloatNum);
+
+ char myFirstText[] = "hello rokeya";
+ printf("%s", myFirstText);
+
+ int num1 = 5;
+ int num2 = 7;
+ int num3 = num1* num2;
+ printf("%d\n", num3);
+
+float floatNumber = 1232.23343432;
+printf("%.3lf", floatNumber);
+
+int result = 5/2;
+float result2 = (float) 5/2;
+printf("\n the result of 5/2 is: %f", result2);
+
+
+
+
+
+
  return 0;
+
+ //this is comment
+ /*
+ this is comment
+ */
  }
