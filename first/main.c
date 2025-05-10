b16c4f269c2726d9be3dd628a9db5b668181992c
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-05-10 20:15:00 +0200

    learning c again after 2019

---
+++ first/main.c
@@ -0,0 +1,12 @@
+
+
+
+#include <stdio.h>
+
+ int main(){
+ int a,b;
+ printf("enter tow numbers: ");
+ printf("here we are taking number from you. \n why we are taking these? \n hodai nitesi bhai. kono karon chra. ");
+ scanf("%d %d", &a,&b);
+ return 0;
+ }
